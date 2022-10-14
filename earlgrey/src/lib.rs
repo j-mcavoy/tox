70b04b7e9674acce56955ee2e9ca9348d63da663
John McAvoy <j-mcavoy@protonmail.com>
2022-10-14 00:10:13 -0700

    pub expose ParseTrees

--- earlgrey/src/lib.rs
+++ earlgrey/src/lib.rs
@@ -1,11 +1,12 @@
 #![deny(warnings)]
 
 mod grammar;
-pub use grammar::{GrammarBuilder, Grammar};
+pub use grammar::{Grammar, GrammarBuilder};
 
 mod items;
 mod parser;
 pub use parser::EarleyParser;
+pub use parser::ParseTrees;
 
 mod trees;
 pub use trees::EarleyForest;
